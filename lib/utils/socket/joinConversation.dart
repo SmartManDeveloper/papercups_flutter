da0c9fca1c633360904992fc366f863a57ff6a47
Adam Pažičan <pazican@smartman.sk>
2021-12-07 12:25:09 +0100

    fix close braces

--- lib/utils/socket/joinConversation.dart
+++ lib/utils/socket/joinConversation.dart
@@ -20,85 +20,71 @@ PhoenixChannel? joinConversationAndListen({
   // Function to set the channel.
   setChannel(conversation);
   // Add the listener that will check for new messages.
-  conversation.messages.listen(
-    (event) {
-      if (event.payload != null) {
-        if (event.payload!["status"] == "error") {
-          // If there is an error, shutdown the channels and remove it.
-          conversation!.close();
-          socket.removeChannel(conversation!);
-          conversation = null;
-        } else {
-          if (event.event.value.contains("shout") || event.event.value.contains("message:created")) {
-            // https://github.com/papercups-io/papercups/pull/488
-            // "message:created" is still not implemented see the PR above.
-            if (event.payload!["customer"] == null)
-              setState!(() {
-                messages!.add(
-                  PapercupsMessage(
-                    accountId: event.payload!["account_id"],
-                    body: event.payload!["body"].toString().trim(),
-                    conversationId: event.payload!["conversation_id"],
-                    customerId: event.payload!["customer_id"],
-                    id: event.payload!["id"],
-                    attachments: (event.payload!["attachments"] != null)
-                        ? (event.payload!["attachments"] as List<dynamic>)
-                            .map((attachment) {
-                            return PapercupsAttachment(
-                              contentType: attachment["content_type"],
-                              fileName: attachment["filename"],
-                              fileUrl: attachment["file_url"],
-                              id: attachment["id"],
-                            );
-                          }).toList()
-                        : null,
-                    fileIds: (event.payload!["attachments"] != null)
-                        ? (event.payload!["attachments"] as List<dynamic>)
-                            .map((attachment) {
-                            return attachment["id"] as String;
-                          }).toList()
-                        : null,
-                    user: (event.payload!["user"] != null)
-                        ? User(
-                            email: event.payload!["user"]["email"],
-                            id: event.payload!["user"]["id"],
-                            role: event.payload!["user"]["role"],
-                            fullName:
-                                (event.payload!["user"]["full_name"] != null)
-                                    ? event.payload!["user"]["full_name"]
-                                    : null,
-                            profilePhotoUrl: (event.payload!["user"]
-                                        ["profile_photo_url"] !=
-                                    null)
-                                ? event.payload!["user"]["profile_photo_url"]
-                                : null,
-                          )
-                        : null,
-                    customer: (event.payload!["customer"] != null)
-                        ? PapercupsCustomer(
-                            email: event.payload!["customer"]["email"],
-                            id: event.payload!["customer"]["id"],
-                          )
-                        : null,
-                    userId: event.payload!["user_id"],
-                    createdAt: event.payload!["created_at"] != null
-                        ? parseDateFromUTC(event.payload!["created_at"])
-                        : null,
-                    seenAt: event.payload!["seen_at"] != null
-                        ? parseDateFromUTC(event.payload!["seen_at"])
-                        : null,
-                    sentAt: event.payload!["sent_at"] != null
-                        ? parseDateFromUTC(event.payload!["sent_at"])
-                        : null,
-                  ),
-                );
-              }, animate: true);
-          }
-            }
-          }
+  conversation.messages.listen((event) {
+    if (event.payload != null) {
+      if (event.payload!["status"] == "error") {
+        // If there is an error, shutdown the channels and remove it.
+        conversation!.close();
+        socket.removeChannel(conversation!);
+        conversation = null;
+      } else {
+        if (event.event.value.contains("shout") || event.event.value.contains("message:created")) {
+          // https://github.com/papercups-io/papercups/pull/488
+          // "message:created" is still not implemented see the PR above.
+          if (event.payload!["customer"] == null)
+            setState!(() {
+              messages!.add(
+                PapercupsMessage(
+                  accountId: event.payload!["account_id"],
+                  body: event.payload!["body"].toString().trim(),
+                  conversationId: event.payload!["conversation_id"],
+                  customerId: event.payload!["customer_id"],
+                  id: event.payload!["id"],
+                  attachments: (event.payload!["attachments"] != null)
+                      ? (event.payload!["attachments"] as List<dynamic>).map((attachment) {
+                          return PapercupsAttachment(
+                            contentType: attachment["content_type"],
+                            fileName: attachment["filename"],
+                            fileUrl: attachment["file_url"],
+                            id: attachment["id"],
+                          );
+                        }).toList()
+                      : null,
+                  fileIds: (event.payload!["attachments"] != null)
+                      ? (event.payload!["attachments"] as List<dynamic>).map((attachment) {
+                          return attachment["id"] as String;
+                        }).toList()
+                      : null,
+                  user: (event.payload!["user"] != null)
+                      ? User(
+                          email: event.payload!["user"]["email"],
+                          id: event.payload!["user"]["id"],
+                          role: event.payload!["user"]["role"],
+                          fullName: (event.payload!["user"]["full_name"] != null)
+                              ? event.payload!["user"]["full_name"]
+                              : null,
+                          profilePhotoUrl: (event.payload!["user"]["profile_photo_url"] != null)
+                              ? event.payload!["user"]["profile_photo_url"]
+                              : null,
+                        )
+                      : null,
+                  customer: (event.payload!["customer"] != null)
+                      ? PapercupsCustomer(
+                          email: event.payload!["customer"]["email"],
+                          id: event.payload!["customer"]["id"],
+                        )
+                      : null,
+                  userId: event.payload!["user_id"],
+                  createdAt:
+                      event.payload!["created_at"] != null ? parseDateFromUTC(event.payload!["created_at"]) : null,
+                  seenAt: event.payload!["seen_at"] != null ? parseDateFromUTC(event.payload!["seen_at"]) : null,
+                  sentAt: event.payload!["sent_at"] != null ? parseDateFromUTC(event.payload!["sent_at"]) : null,
+                ),
+              );
+            }, animate: true);
         }
       }
-    },
-  );
+    }
+  });
   return conversation;
 }
